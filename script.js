b71da72eac2a7987640e852a9e617439d085167d
ananthu <ananthutr2004@gmail.com>
2025-09-28 01:30:58 +0530

    Your message

---
+++ script.js
@@ -0,0 +1,13 @@
+function SignUp(){
+    let name=document.getElementById('name').value
+    let email=document.getElementById('S-Email').value
+    let pass=document.getElementById('S-pass').value
+    console.log(name,email,pass);
+}
+
+function Login(){
+    let email=document.getElementById('email').value
+    let pass=document.getElementById('pass').value
+    console.log(email,pass);
+    
+}
